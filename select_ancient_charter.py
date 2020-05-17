2602dc30509d72def7663f446f9f9a8d1d1e3ae9
aicfe <674361437@qq.com>
2020-05-17 22:14:06 +0800

    fix

--- select_ancient_charter.py
+++ select_ancient_charter.py
@@ -2,7 +2,7 @@
 # _*_ coding:utf-8 _*_
 import os
 import json
-
+import xlwt
 top_idx = 500 # 考虑前500个最常出现的字
 
 
@@ -27,10 +27,15 @@ def analysis_ancient_name(path):
             else:
                 all_chart[name[charter_idx]] +=1
     sort_charter = sorted(all_chart.items(), key=lambda x: x[1], reverse=True)
+    rest_chater = sort_charter[top_idx:]
+    rest_ancient_list = []
+    for key in rest_chater:
+        rest_ancient_list.append(key[0])
     sort_charter = sort_charter[:top_idx]
+
     with open('charter.json', 'w', encoding='utf-8') as json_write:
         json.dump(sort_charter, json_write, indent=4, ensure_ascii=False)
-    return sort_charter
+    return sort_charter,rest_ancient_list
 
 def analysis_modern_name(path):
     data = get_data(path)
@@ -64,12 +69,39 @@ def sele_comment(ancient, modern):
     # print(ancient_list)
     # print(modern_list)
     print(comment)
+    return ancient_list,modern_list,comment
+
+def save_excel(data_list,index):
+    # 创建一个workbook 设置编码
+    workbook = xlwt.Workbook(encoding='utf-8')
+    # 创建一个worksheet
+    worksheet = workbook.add_sheet('My Worksheet')
+
+    # 写入excel
+    # 参数对应 行, 列, 值
+    for idx in range(len(data_list)):
+        worksheet.write(idx, index, data_list[idx])
+
+    # 保存
+    workbook.save('Excel_test'+ str(index) +'.xls')
 
 if __name__ =='__main__':
     path = './Chinese_Names_Corpus/Ancient_Names_Corpus（25W）.txt'
-    ancient = analysis_ancient_name(path)
+    ancient,rest_ancient = analysis_ancient_name(path)
 
     path2 = './Chinese_Names_Corpus/Chinese_Names_Corpus（120W）.txt'
     modern = analysis_modern_name(path2)
 
-    sele_comment(ancient, modern)
+    ancient_list, modern_list, comment =sele_comment(ancient, modern)
+    ancient_rest,modern_rest = [],[]
+    for tmp in ancient_list:
+        if tmp not in comment:
+            ancient_rest.append(tmp)
+    for tmp2 in modern_list:
+        if tmp2 not in comment:
+            modern_rest.append(tmp2)
+
+    # save_excel(ancient_rest,1)
+    # save_excel(modern_rest,2)
+    # save_excel(comment,3)
+    save_excel(rest_ancient,4)
